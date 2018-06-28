579007ae85acf338510f9b3eb2cbde71c1ad518b
Patrick Mutwiri <patwiri@gmail.com>
2018-06-28 10:11:15 +0300

    Simple Calc

---
+++ patricmutwiri/calculator/src/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Patricmutwiri\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
